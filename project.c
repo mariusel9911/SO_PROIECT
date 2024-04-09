9f5eccc1075ceb239ee73b74f7e920fa3ab20d08
Marius <marius.nistor@student.upt.ro>
2024-04-10 00:45:45 +0300

    Verify if first argument is a directory and if it is you'll open it

--- project.c
+++ project.c
@@ -1,11 +1,30 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
 #include <stdio.h>
-#include <errno.h>
+#include <stdlib.h>
+#include <sys/sysmacros.h>
 
 int main(int argc, char *argv[]){
 
+    struct stat sb;
+
+    /// Verificare numar argumente
     if (argc != 2){
 
-        perror("Invalid number of arguments!");
+        fprintf(stderr, "Invalid number of arguments! Usage: %s <dir_name>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    /// Lstat citim datele lui argv[0] in sb (detalii in man 2 lstat / lab 4)
+    if (lstat(argv[1], &sb) < 0){
+        perror("lstat");
+        exit(EXIT_FAILURE);
+    }
+
+    /// Din sb extragem st_mode si folosim macro pt a verifica daca este un diretor (lab 4 jos de tot)
+    if (!S_ISDIR(sb.st_mode)){
+        fprintf(stderr, "%s is not a directory\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
